c96c5a437aeb50faffbeb6238f81022b48df341b
Phuong <0306231233@caothang.edu.vn>
2026-09-07 14:03:19 +0700

    fix: delete Payload Website Template

--- tests/e2e/frontend.spec.ts
+++ tests/e2e/frontend.spec.ts
@@ -4,7 +4,6 @@ test.describe('Frontend', () => {
   test('can load homepage', async ({ page }) => {
     await page.goto('http://localhost:3000')
     await expect(page).toHaveTitle(/Payload Website Template/)
-    const heading = page.locator('h1').first()
-    await expect(heading).toHaveText('Payload Website Template')
+    await expect(page.locator('h2').first()).toHaveText('Các khóa học nổi bật')
   })
 })
